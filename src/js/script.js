742c195983582c876fd33f51ce93cf9fb0b88004
あなたの名前 <>
2023-10-20 11:10:55 +0900

    ヘッダーまで終了

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,14 @@
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+$(function ($) { // この中であればWordpressでも「$」が使用可能になる
+	$("#js-drawer-icon").on("click", function (e) {
+		e.preventDefault();
+		$("#js-drawer-icon").toggleClass("is-active");
+		$("#js-drawer-content").toggleClass("is-active");
+		$("#js-header").toggleClass("is-active");
+	})
 
+	$('#js-drawer-content a[href^="#"]').on('click', function () {
+		$("#js-drawer-icon, #js-drawer-content").removeClass("is-active");
+	});
 
 });
