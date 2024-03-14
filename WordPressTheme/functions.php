dba7dbdc4aff920f134115ba69c94af5356487a2
あなたの名前 <>
2024-03-14 14:43:06 +0900

    WordPress化

---
+++ WordPressTheme/functions.php
@@ -0,0 +1,41 @@
+<?php
+function my_setup()
+{
+    add_theme_support('post-thumbnails'); // アイキャッチ画像を有効化
+    add_theme_support('automatic-feed-links'); // 投稿とコメントのRSSフィードのリンクを有効化
+    add_theme_support('title-tag'); // titleタグ自動生成
+    add_theme_support('html5', array( // HTML5による出力
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+    ));
+}
+add_action('after_setup_theme', 'my_setup');
+
+
+/* CSSとJavaScriptの読み込み */
+function my_script_init()
+{ // WordPressに含まれているjquery.jsを読み込まない
+    wp_deregister_script('jquery');
+    // Google Fonts
+    wp_enqueue_style('NotoSansJP', '//fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap');
+    wp_enqueue_style('Oswald', '//fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap');
+    wp_enqueue_style('Robots', '//fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+    // Font Awesome
+    wp_enqueue_script('font-awesome', 'https://kit.fontawesome.com/6ec95a7f03.js', array(), "1.0.1", true);
+    // Swiper JS
+    wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array('jquery'), "1.0.1", true);
+    // jQuery Inview
+    wp_enqueue_script('jquery-inview', get_template_directory_uri() . '/assets/js/jquery.inview.min.js', array('jquery'), "1.0.1", true);
+    // jQuery
+    wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.6.0.js', array(), "1.0.1", true);
+    // Custom Script
+    wp_enqueue_script('custom-script', get_template_directory_uri() . '/assets/js/script.js', array('jquery'),  filemtime( get_theme_file_path( '/assets/js/script.js' )), true);
+    // Swiper CSS
+    wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', "", "1.0.1");
+    // Custom Style
+    wp_enqueue_style('style-css', get_template_directory_uri() . '/assets/css/style.css', array(), filemtime( get_theme_file_path( '/assets/css/style.css' )));
+}
+add_action('wp_enqueue_scripts', 'my_script_init');
