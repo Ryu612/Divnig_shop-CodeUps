ee11026107f41bdc42dfa941b35c864eaf6ad5a2
あなたの名前 <>
2024-03-23 13:05:31 +0900

    campaign-card__label修正

--- WordPressTheme/archive-campaign.php
+++ WordPressTheme/archive-campaign.php
@@ -101,14 +101,16 @@ $the_query = new WP_Query($args);
 						</div>
 						<div class="campaign-card__contents campaign-card__contents--sub">
 							<div class="campaign-card__head campaign-card__head--sub">
-								<?php
-								$taxonomy_terms = get_the_terms($post->ID, 'campaign_category');
-								if (!empty($taxonomy_terms)) {
-									foreach ($taxonomy_terms as $taxonomy_term) {
-										echo '<div class="campaign-card__label">' . esc_html($taxonomy_term->name) . '</div>';
+								<div class="campaign-card__category">
+									<?php
+									$taxonomy_terms = get_the_terms($post->ID, 'campaign_category');
+									if (!empty($taxonomy_terms)) {
+										foreach ($taxonomy_terms as $taxonomy_term) {
+											echo '<div class="campaign-card__label">' . esc_html($taxonomy_term->name) . '</div>';
+										}
 									}
-								}
-								?>
+									?>
+								</div>
 								<h3 class="campaign-card__title campaign-card__title--sub"><?php the_title(); ?></h3>
 							</div>
 							<div class="campaign-card__price-body campaign-card__price-body--sub">
