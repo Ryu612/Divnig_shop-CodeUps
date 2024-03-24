48a5bc8678b48b15155e7a2ccad6e4dc63f1cea8
あなたの名前 <>
2024-03-24 13:25:55 +0900

    サイトマップのリンク切れ修正

--- WordPressTheme/page-sitemap.php
+++ WordPressTheme/page-sitemap.php
@@ -1,3 +1,18 @@
+<?php
+$home = esc_url(home_url('/'));
+$campaign = esc_url(home_url('/campaign/'));
+$about = esc_url(home_url('/about-us/'));
+$information = esc_url(home_url('/information/'));
+$blog = esc_url(home_url('/blog/'));
+$voice = esc_url(home_url('/voice/'));
+$price = esc_url(home_url('/price/'));
+$faq = esc_url(home_url('/faq/'));
+$contact = esc_url(home_url('/contact/'));
+$policy = esc_url(home_url('/privacy-policy/'));
+$tos = esc_url(home_url('/terms-of-service/'));
+$sitemap = esc_url(home_url('/sitemap/'));
+?>
+
 <?php get_header(); ?>
 	<!-- メインビュー -->
 	<div class="sub-mv">
@@ -29,9 +44,9 @@
 			<div class="footer-nav__1">
 				<ul class="footer-nav__items">
 					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#campaign">キャンペーン</a></li>
-					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#">ライセンス取得</a></li>
-					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#">貸切体験ダイビング</a></li>
-					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#">ナイトダイビング</a></li>
+					<li class="footer-nav__item footer-nav__item--sitemap"><a href="<?php echo esc_url(home_url('/campaign_category/ファンダイビング')); ?>">ファンダイビング</a></li>
+					<li class="footer-nav__item footer-nav__item--sitemap"><a href="<?php echo esc_url(home_url('/campaign_category/ライセンス講習')); ?>">ライセンス講習</a></li>
+					<li class="footer-nav__item footer-nav__item--sitemap"><a href="<?php echo esc_url(home_url('/campaign_category/体験ダイビング')); ?>">体験ダイビング</a></li>
 				</ul>
 				<ul class="footer-nav__items">
 					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#about">私たちについて</a></li>
@@ -40,40 +55,40 @@
 			<div class="footer-nav__2">
 				<ul class="footer-nav__items">
 					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#information">ダイビング情報</a></li>
-					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#">ライセンス講習</a></li>
-					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#">体験ダイビング</a></li>
-					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#">ファンダイビング</a></li>
+					<li class="footer-nav__item footer-nav__item--sitemap"><a href="<?php echo $information; ?>#tab1">ライセンス講習</a></li>
+					<li class="footer-nav__item footer-nav__item--sitemap"><a href="<?php echo $information; ?>#tab2">ファンダイビング</a></li>
+					<li class="footer-nav__item footer-nav__item--sitemap"><a href="<?php echo $information; ?>#tab3">体験ダイビング</a></li>
 				</ul>
 				<ul class="footer-nav__items">
-					<li class="footer-nav__item footer-nav__item--sitemap"><a href="home.html">ブログ</a></li>
+					<li class="footer-nav__item footer-nav__item--sitemap"><a href="<?php echo $blog; ?>">ブログ</a></li>
 				</ul>
 			</div>
 			<div class="footer-nav__3 footer-nav__3--sitemap">
 				<ul class="footer-nav__items">
-					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#voice">お客様の声</a></li>
+					<li class="footer-nav__item footer-nav__item--sitemap"><a href="<?php echo $voice; ?>">お客様の声</a></li>
 				</ul>
 				<ul class="footer-nav__items">
-					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#price">料金一覧</a></li>
-					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#">ライセンス講習</a></li>
-					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#">体験ダイビング</a></li>
-					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#">ファンダイビング</a></li>
+					<li class="footer-nav__item footer-nav__item--sitemap"><a href="<?php echo $price; ?>">料金一覧</a></li>
+					<li class="footer-nav__item footer-nav__item--sitemap"><a href="<?php echo $price; ?>#price-license">ライセンス講習</a></li>
+					<li class="footer-nav__item footer-nav__item--sitemap"><a href="<?php echo $price; ?>#price-experience">体験ダイビング</a></li>
+					<li class="footer-nav__item footer-nav__item--sitemap"><a href="<?php echo $price; ?>#price-fun">ファンダイビング</a></li>
 				</ul>
 			</div>
 			<div class="footer-nav__4 footer-nav__4--sitemap">
 				<ul class="footer-nav__items">
-					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#">よくある質問</a></li>
+					<li class="footer-nav__item footer-nav__item--sitemap"><a href="<?php echo $faq; ?>">よくある質問</a></li>
 				</ul>
 				<ul class="footer-nav__items">
-					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#">プライバシー<br class="u-mobile">ポリシー</a></li>
+					<li class="footer-nav__item footer-nav__item--sitemap"><a href="<?php echo $policy; ?>">プライバシー<br class="u-mobile">ポリシー</a></li>
 				</ul>
 				<ul class="footer-nav__items">
-					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#">利用規約</a></li>
+					<li class="footer-nav__item footer-nav__item--sitemap"><a href="<?php echo $tos; ?>">利用規約</a></li>
 				</ul>
 				<ul class="footer-nav__items">
-					<li class="footer-nav__item footer-nav__item--sitemap"><a href="#contact">お問いわ合せ</a></li>
+					<li class="footer-nav__item footer-nav__item--sitemap"><a href="<?php echo $contact; ?>">お問いわ合せ</a></li>
 				</ul>
 				<ul class="footer-nav__items">
-					<li class="footer-nav__item footer-nav__item--sitemap"><a href="page-sitemap.html">サイトマップ</a></li>
+					<li class="footer-nav__item footer-nav__item--sitemap"><a href="<?php echo $sitemap; ?>">サイトマップ</a></li>
 				</ul>
 			</div>
 		</div>
