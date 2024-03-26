cf0a0903b04a0c1322a430719642ee380027ebd7
あなたの名前 <>
2024-03-26 15:10:31 +0900

    voice_categoryの表示件数設定

--- WordPressTheme/functions.php
+++ WordPressTheme/functions.php
@@ -98,7 +98,7 @@ function custom_posts_per_page2($query)
 {
     if (!is_admin() && $query->is_main_query()) {
         // カスタム投稿のスラッグを記述
-        if (is_post_type_archive('voice') || is_tax('campaign_voice')) {
+        if (is_post_type_archive('voice') || is_tax('voice_category')) {
             // 表示件数を指定
             $query->set('posts_per_page', 6);
         }
